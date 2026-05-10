6764a786f2b8cc4ed3f51aab4f2522b082eb5a8d
Saleh <salehenab850@gmail.com>
2026-05-10 23:18:55 +0300

    refactor: move db code into separated package to avoid dependency cycles

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,23 +1,23 @@
-package app
+package db
 
 import "sync"
 
-var Data = &kvStore{m: make(map[string]string)}
+var Data = &kvStore{M: make(map[string]string)}
 
 type kvStore struct {
 	mu sync.RWMutex
-	m  map[string]string
+	M  map[string]string
 }
 
 func (s *kvStore) Get(key string) (string, bool) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	v, ok := s.m[key]
+	v, ok := s.M[key]
 	return v, ok
 }
 
 func (s *kvStore) Set(key, val string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	s.m[key] = val
+	s.M[key] = val
 }
